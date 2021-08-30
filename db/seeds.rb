83cbf92146f4c94481413f02ed48071fada9e741
mokastinkey <mocastinkey@gmail.com>
2021-08-30 13:00:19 +0000

    [Add]seed.rbにuserモデルのデータを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  nickname: 'テスト',
+  email: 'test@test.com',
+  password: 'testtest',
+  profile_image_id: File.open("./app/assets/images/test.jpg"),
+  self_introduction: 'これはテストです。'
+)
