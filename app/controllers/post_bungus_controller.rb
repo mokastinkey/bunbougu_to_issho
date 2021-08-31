6d31b538ebb259cced95c633a7d4f9d3102835eb
mokastinkey <mocastinkey@gmail.com>
2021-08-31 05:40:29 +0000

    [Add]postbunguのnewhtml項目作成

--- app/controllers/post_bungus_controller.rb
+++ app/controllers/post_bungus_controller.rb
@@ -4,7 +4,24 @@ class PostBungusController < ApplicationController
     @post_bungus = PostBungu.all
   end
 
+  def new
+    @post_bungu = PostBungu.new
+  end
+
   def show
+  end
+
+  private
+
+  def post_bungu_params
+    params.require(:post_bungu).permit(
+      :catchphrase,
+      :bungu_images_images,
+      :thought,
+      :manufacturer,
+      :price,
+      :place
+    )
   end
 
 end
