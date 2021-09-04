85138c40a210e3bf98fae981a03d0e4862ae3407
mokastinkey <mocastinkey@gmail.com>
2021-09-04 04:18:38 +0000

    [Add]relationshipsコントローラ作成/モデルに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,30 @@ class User < ApplicationRecord
   has_many :post_bungus, dependent: :destroy
   attachment :profile_image
 
+  belongs_to :follower, class_name: "User"
+  belongs_to :following, class_name: "User"
+  validates :follower_id, presence: true
+  validates :following_id, presence: true
+
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
+  has_many :following, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+  #フォローしているかを確認するメソッド
+  def following?(user)
+    following_relationships.find_by(following_id: user.id)
+  end
+
+  #フォローするときのメソッド
+  def follow(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  #フォローを外すときのメソッド
+  def unfollow(user)
+    following_relationships.find_by(following_id: user.id).destroy
+  end
+
+
 end
