b7e135089f4335848f6928d1b80180d33f165b18
mokastinkey <mocastinkey@gmail.com>
2021-09-04 05:11:37 +0000

    [Add]フォロー機能ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :post_bungus
+
 end
