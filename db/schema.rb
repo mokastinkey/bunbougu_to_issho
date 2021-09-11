dcddb311ce473bc6b2d547376862f77b2bd7aa90
mokastinkey <mocastinkey@gmail.com>
2021-09-11 06:14:19 +0000

    [Add]postcommentモデル/テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_030059) do
+ActiveRecord::Schema.define(version: 2021_09_11_061040) do
 
   create_table "bungu_images", force: :cascade do |t|
     t.integer "post_bungu_id", null: false
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 2021_09_06_030059) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "other_manufacturer"
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "post_bungu_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "relationships", force: :cascade do |t|
