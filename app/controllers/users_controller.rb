8d229730b81dad85ac7253c630c2e4eb900ca8e7
mokastinkey <mocastinkey@gmail.com>
2021-09-24 04:59:15 +0000

    [Add]フォローフォロワー一覧にkaminari導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,14 @@ class UsersController < ApplicationController
     @login_user = current_user
     #自分がフォローしているユーザー一覧
     @user  = User.find(params[:id])
-    @users = @user.following_user.where.not(id: current_user.id)
+    @users = @user.following_user.where.not(id: current_user.id).page(params[:page]).per(1)
   end
 
   def follower
     @login_user = current_user
     #@userをフォローしているユーザー
     @user = User.find(params[:id])
-    @users = @user.follower_user
+    @users = @user.follower_user.page(params[:page]).per(1)
     # 上の続き where.not(id: current_user.id)
   end
 
