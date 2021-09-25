9e05f735a430eb3051362106141b382a2c44bc7b
mokastinkey <mocastinkey@gmail.com>
2021-09-25 03:29:19 +0000

    [FIx]スライドショー矢印やnav-blockの大きさ・幅を小さく修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,17 +23,17 @@
 document.addEventListener("turbolinks:load", function () {
 
   $(document).ready(function(){
-    
+
     $("#nav-button").on("click", function(){
        $(".sp-nav").slideToggle();
      　$("#mask").toggleClass('active')
     });
-    
+
     $("#slide-images").skippr({
     // スライドショーの変化 ("fade" or "slide")
-    transition : 'slide',
+    transition : 'fade',
     // 変化に係る時間(ミリ秒)
-    speed : 1000,
+    speed : 1500,
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形("block" or "bubble")
@@ -45,14 +45,14 @@ document.addEventListener("turbolinks:load", function () {
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 3000,
+    autoPlayDuration : 5000,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
     });
-    
+
   });
-    
+
 });
 
