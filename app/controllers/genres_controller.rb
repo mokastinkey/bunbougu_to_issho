dedaabbefc020611199bc6b3d665ccb64625e256
mokastinkey <mocastinkey@gmail.com>
2021-09-25 06:37:03 +0000

    [Fix]ジャンル別一覧の表示順をcreated_at descに変更

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,6 +2,6 @@ class GenresController < ApplicationController
   def show
     @genres = Genre.all
     @genre = Genre.find(params[:id])
-    @post_bungus = @genre.post_bungus
+    @post_bungus = @genre.post_bungus.order(created_at: "desc")
   end
 end
