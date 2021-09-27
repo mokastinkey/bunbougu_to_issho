8782cc8ba5949acfe05555755faa98ba6c8de910
mokastinkey <mocastinkey@gmail.com>
2021-09-27 01:24:35 +0000

    [Fix]genre検索画面にkaminariでページネーション追加

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,6 +2,6 @@ class GenresController < ApplicationController
   def show
     @genres = Genre.all
     @genre = Genre.find(params[:id])
-    @post_bungus = @genre.post_bungus.order(created_at: "desc")
+    @post_bungus = @genre.post_bungus.page(params[:page]).reverse_order.per(30)
   end
 end
